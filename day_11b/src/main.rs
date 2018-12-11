30b682f8261dddca0ce8a2f2d34fe769bef84274
yen <yenthefirst@gmail.com>
2018-12-10 23:31:27 -0800

    day 11 optimized

--- day_11b/src/main.rs
+++ day_11b/src/main.rs
@@ -4,39 +4,33 @@ fn main() {
     a.next();
     let serial = a.next().unwrap().parse::<i32>().unwrap();
 
-    let best = (0..(300*300*300)).max_by_key(|xyn| {
-        let x = xyn % 300;
-        let y = (xyn / 300) % 300;
-        let n = xyn / 300 / 300;
-        //ok actualllyyyyyy there's an off-by-one here, since grid is 1...300 inclusive, not
-        //0...299 inclusive. it's probably fine.
-        //it worked fine for part one, so eh.
-        if (x+n) > 300 || (y+n) > 300 {
-            0
-        } else {
-            (0..n).map(|sy| {
-                (0..n).map(|sx| {
-                    let rack_id = (x+sx)+10;
-                    let p = ((rack_id * (y+sy)) + serial) * rack_id;
-                    (p / 100) % 10 - 5
-                }).sum::<i32>()
-            }).sum::<i32>()
-        }
+    let grid : Vec<Vec<i32>> = std::iter::once(vec![0;301]).chain((1..300+1).map(|y| {
+        std::iter::once(0).chain((1..300+1).map(move |x| {
+            let rack_id = x+10;
+            let p = ((rack_id * y) + serial) * rack_id;
+            (p / 100) % 10 - 5
+        })).collect()
+    })).collect();
+
+    let sums : Vec<Vec<i32>> = grid.iter().map(|row| {
+        row.iter().scan(0, |sum, cell| {
+            *sum = *sum + cell;
+            Some(*sum)
+        })
+    }).scan(vec![0;301], |sumrow, row| {
+        *sumrow = sumrow.iter().zip(row).map(|(a,b)| a+b).collect();
+        Some(sumrow.clone())
+    }).collect();
+
+    let (x,y,n) = (1..300+1).flat_map(|n| {
+        (1..300-n+1).flat_map(move |y| {
+            (1..300-n+1).map(move |x| {
+                (x,y,n)
+            })
+        })
+    }).max_by_key(|&(x,y,n)| {
+        sums[y+n-1][x+n-1] - sums[y+n-1][x-1] - sums[y-1][x+n-1] + sums[y-1][x-1]
     }).unwrap();
 
-    let x = best % 300;
-    let y = (best / 300) % 300;
-    let n = best / 300 / 300;
     println!("{},{},{}", x,y,n);
-
-    /*(-1..n+1).for_each(|sy| {
-        (-1..n+1).for_each(|sx| {
-            let rack_id = (x+sx)+10;
-            let p = ((rack_id * (y+sy)) + serial) * rack_id;
-            let pow = (p / 100) % 10 - 5;
-            print!("{:2} ",pow);
-        });
-        println!("");
-    })*/
-
 }
